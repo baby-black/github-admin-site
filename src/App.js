34d29019098cc5ebf636760e61f91cdca9200315
baby-black <den101472@donga.edu.vn>
2024-11-20 00:02:31 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import '../src/styles.css';  // Import CSS vào
+
+import SearchBar from './components/SearchBar';
+import RepoList from './components/RepoList';
+import ErrorMessage from './components/ErrorMessage';
+import { getUserRepos } from './services/githubService';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [projectsCount, setProjectsCount] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [error, setError] = useState(null);
+
+  const handleSearch = async (username) => {
+    setError(null);
+    setProjectsCount(null);
+    setRepos([]); // Clear repos on new search
+    try {
+      const repos = await getUserRepos(username);
+      setRepos(repos);
+      setProjectsCount(repos.length);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Navbar */}
+      <div className="navbar">
+        <div className="logo">GitHub Admin</div>
+        <div>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
+
+      <div className="App-header">
+        <h1>Search GitHub User</h1>
+        <SearchBar onSearch={handleSearch} />
+        {error && <ErrorMessage message={error} />}
+        {projectsCount !== null && (
+          <RepoList 
+            count={projectsCount} 
+            repos={repos} 
+            username={username}
+          />
+        )}
+      </div>
     </div>
   );
 }
